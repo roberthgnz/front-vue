87d620711998e139b9a037b35ababc462641e0fc
rxb3rth <reliutg@gmail.com>
2022-12-26 19:29:43 +0100

    protect

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,26 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./pages/Home.vue";
 import SignIn from "./pages/SignIn.vue";
 import SignUp from "./pages/SignUp.vue";
-import Account from "./pages/Account.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/sign-in", component: SignIn },
   { path: "/sign-up", component: SignUp },
-  { path: "/account", component: Account },
 ];
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ["/sign-in", "/sign-up"];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem("userSession");
+
+  if (authRequired && !loggedIn) {
+    return next("/sign-in");
+  }
+
+  next();
 });
